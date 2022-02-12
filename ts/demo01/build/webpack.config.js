e0243b991d9c11c030557b6f812f7002a078db08
hdouhua <hdouhua@gmail.com>
2022-02-12 15:13:11 +0800

    [fix]: the way to get webpack mode

--- ts/demo01/build/webpack.config.js
+++ ts/demo01/build/webpack.config.js
@@ -4,12 +4,14 @@ const baseConfig = require('./webpack.base.config')
 const devConfig = require('./webpack.dev.config')
 const proConfig = require('./webpack.pro.config')
 
-// 1)
+// // 1) wrong way!!!
+// console.log('mode:', process.NODE_ENV) // undefined
 // let config = process.NODE_ENV === 'development' ? devConfig : proConfig
 // module.exports = merge(baseConfig, config)
 
-// OR 2)
+// 2) correct way
 module.exports = (env, argv) => {
+  console.log('mode:', argv.mode)
   let config = argv.mode === 'development' ? devConfig : proConfig;
 
   return merge(baseConfig, config);
