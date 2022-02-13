7e94ff517094247eff39a3b9f1f81659a9f87378
hdouhua <hdouhua@gmail.com>
2022-02-14 00:51:02 +0800

    [change]: merge practice demo code

---
+++ ts/ts-cra/src/components/demo/HelloHOC.tsx
@@ -0,0 +1,32 @@
+//
+// 高阶组件
+// React 类型检查没有很好的支持高阶组件，更推荐使用 Hook 来实现。
+//
+
+import React, { Component } from "react";
+
+import HelloClass from './HelloClass'
+
+interface Loading {
+  loading: boolean;
+}
+
+// // HOC 组件的基本型
+// function HelloHOC(WrappedComponent: React.ComponentType) {
+//   return class extends Component {
+//     render() {
+//         return <></>
+//     }
+//   }
+// }
+//=> 加入泛型约束
+function HelloHOC<P>(WrappedComponent: React.ComponentType<P>) {
+  return class extends Component<P & Loading> {
+    render() {
+      const { loading, ...props } = this.props
+      return loading ? <div>loading ...</div> : <WrappedComponent {...props as P} />
+    }
+  }
+}
+
+export default HelloHOC(HelloClass)
--- ts/ts-cra/src/index.tsx
+++ ts/ts-cra/src/index.tsx
@@ -3,13 +3,19 @@ import ReactDOM from "react-dom"
 
 import reportWebVitals from './reportWebVitals'
 import ErrorBoundary from './components/ErrorBoundary'
-import RootRouter from './routers/index'
+
+// import Hello from "./components/demo/Hello"
+// import Hello from "./components/demo/HelloClass"
+// import Hello from "./components/demo/HelloHOC"
+// import Hello from "./components/demo/HelloHooks"
 // import App from './components/App';
+import RootRouter from './routers/index'
 
 reportWebVitals(console.log)
 
 ReactDOM.render(
   <ErrorBoundary>
+    {/* <Hello name="Typescript" /> */}
     <RootRouter />
   </ErrorBoundary>,
   document.querySelector('.app')
