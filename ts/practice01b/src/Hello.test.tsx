8087100c38376c32851ee0edb394aae1a9c6487e
hdouhua <hdouhua@gmail.com>
2022-02-14 00:43:08 +0800

    [delete]: merge the project to ts-cra

--- ts/practice01b/src/Hello.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Hello from './components/demo/Hello';
-
-test('renders learn react link', () => {
-  render(<Hello name='Typescript' />);
-  const element = screen.getByText(/hello typescript/i);
-  expect(element).toBeInTheDocument();
-});
