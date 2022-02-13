a32575812d6496994e049ae478de592e2a14bbda
hdouhua <hdouhua@gmail.com>
2022-02-13 12:47:17 +0800

    [add]: enable report web-vitals

--- ts/practice01b/src/index.tsx
+++ ts/practice01b/src/index.tsx
@@ -7,7 +7,9 @@ import ReactDOM from "react-dom"
 // import Hello from "./components/demo/HelloClass"
 // import Hello from "./components/demo/HelloHOC"
 import Hello from "./components/demo/HelloHooks"
+import reportWebVitals from './reportWebVitals'
 
+reportWebVitals(console.log)
 
 ReactDOM.render(
   <Hello name="Typescript" />,
